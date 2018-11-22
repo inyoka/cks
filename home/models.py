8e0fdaf0b09a08f4339ba14bd1cb9248d0f9f7dc
Simon <simonmarkholland@gmail.com>
2018-11-22 14:46:49 +0700

    HomePage with streamfield model.

--- home/models.py
+++ home/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class HomePage(Page):
-    pass
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body', classname="full"),
+    ]
